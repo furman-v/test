27a22716760d7be362bb0454c3caa89f2796be05
Mac <mac@MacBook-Pro-Mac.local>
2020-02-23 16:55:33 +0200

    added javascript

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,14 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const a = 5;
+const b = 10;
+const sum = a + b;
+const min = a - b;
+const div = a / b;
+const mult = a * b;
+
+console.log(sum);
+console.log(min);
+console.log(div);
+console.log(mult);
+
