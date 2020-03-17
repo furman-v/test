3189b7d55a52750af10318763e5f77150b473e89
Mac <mac@MacBook-Pro-Mac.local>
2020-03-18 00:25:54 +0200

    first page using bootstrap

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,13 +1,13 @@
-function getVisitorsCountries() {
-  console.log("Got visitor's county");
-}
+// function getVisitorsCountries() {
+//   console.log("Got visitor's county");
+// }
 
-function buttonClick() {
-  alert("DONE");
-}
-function aboutTheFirstAlbum() {
-  alert("This is the band's fist album");
-}
-window.getVisitorsCountries = getVisitorsCountries;
-window.buttonClick = buttonClick;
-window.aboutTheFirstAlbum = aboutTheFirstAlbum;
+// function buttonClick() {
+//   alert("DONE");
+// }
+// function aboutTheFirstAlbum() {
+//   alert("This is the band's fist album");
+// }
+// window.getVisitorsCountries = getVisitorsCountries;
+// window.buttonClick = buttonClick;
+// window.aboutTheFirstAlbum = aboutTheFirstAlbum;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,40 @@
 import "../styles/index.scss";
-import "./events";
-const a = 5;
-const b = 10;
-const sum = a + b;
-const min = a - b;
-const div = a / b;
-const mult = a * b;
+import "bootstrap";
+//import "./events";
+// const a = 5;
+// const b = 10;
+// const sum = a + b;
+// const min = a - b;
+// const div = a / b;
+// const mult = a * b;
 
-console.log(sum);
-console.log(min);
-console.log(div);
-console.log(mult);
+// console.log(sum);
+// console.log(min);
+// console.log(div);
+// console.log(mult);
 
-const user = {
-  name: "Victoria",
-  surname: "Furman",
-  age: 21,
-  dateOfBirth: "26th of December",
-  height: 160,
-  havePets: true,
-  favFood: ["coffee", "buckwheat", "meat", "avocado"],
-  logNameAndDateOfBirth: function() {
-    console.log(
-      "Info about the user",
-      this.name + " " + this.surname + " " + this.dateOfBirth
-    );
-  }
-};
+// const user = {
+//   name: "Victoria",
+//   surname: "Furman",
+//   age: 21,
+//   dateOfBirth: "26th of December",
+//   height: 160,
+//   havePets: true,
+//   favFood: ["coffee", "buckwheat", "meat", "avocado"],
+//   logNameAndDateOfBirth: function() {
+//     console.log(
+//       "Info about the user",
+//       this.name + " " + this.surname + " " + this.dateOfBirth
+//     );
+//   }
+// };
 
-console.log(user.name);
-console.log(user.surname);
-console.log(user.age);
-console.log(user.dateOfBirth);
-console.log(user.height);
-console.log(user.havePets);
-console.log(user.favFood);
+// console.log(user.name);
+// console.log(user.surname);
+// console.log(user.age);
+// console.log(user.dateOfBirth);
+// console.log(user.height);
+// console.log(user.havePets);
+// console.log(user.favFood);
 
-user.logNameAndDateOfBirth();
+// user.logNameAndDateOfBirth();
